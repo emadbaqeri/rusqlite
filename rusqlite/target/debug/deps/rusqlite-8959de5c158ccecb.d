d1df8c5ebee44333031f7e3262c6fe414fcdab7a
emad.baqeri <emad.bagheri@g2holding.org>
2025-01-22 00:33:23 +0330

    refactor(core): initiate project with workspace architecture of rust

--- rusqlite/target/debug/deps/rusqlite-8959de5c158ccecb.d
+++
@@ -1,5 +0,0 @@
-/home/adamvizly/projects/rusqlite/rusqlite/target/debug/deps/librusqlite-8959de5c158ccecb.rmeta: src/main.rs
-
-/home/adamvizly/projects/rusqlite/rusqlite/target/debug/deps/rusqlite-8959de5c158ccecb.d: src/main.rs
-
-src/main.rs:
